6173e7ecec9817b74d1f0f1d60b6d54912b7ba9a
dana <handany1102@gmail.com>
2025-04-13 17:23:13 +0900

    remove commit

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -6,16 +6,24 @@ interface IParams {
 	params: {id: string};
 }
 
-export async function generateMetadata({ params: { id } }: IParams) {
+export async function generateMetadata({
+	params,
+  }: {
+	params: Promise<{ id: string }>
+  }) {
+	const {id} = await params;
 	const movie = await getMovie(id);
 	return {
 	  title: movie.title,
 	};
   }
 
-export default async function MovieDetailPage({
-    params: { id },
-}: IParams) {
+export default async function MovieDetailPage( {
+	params,
+  }: {
+	params: Promise<{ id: string }>
+  }) {
+	const {id} = await params;
     return (
         <div>
             <Suspense fallback={<h1>Loading movie Info</h1>}>
